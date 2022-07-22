00dbb23b349001b523a0cf7c99fd85b6dd1733f5
Daniel Wang <danwyk@Daniel-MacBook-Air.local>
2022-07-22 18:48:40 -0400

    Finish Question 3

--- 3_sortedSquareArray_V2.cpp
+++ 3_sortedSquareArray_V2.cpp
@@ -4,33 +4,53 @@ using namespace std;
 
 vector<int> sortedSquaredArray(vector<int> &array) {
     // Write your code here.
-    // O(nlogn)
+    // O(n)
 
     vector<int> new_array;
-    bool negativeNum = false;
     new_array.reserve(array.size());
 
-    for(int i = 0; i < array.size(); ++i) {
-    
-        if(array[i] < 0) {
-            negativeNum = true;
-        }
-        new_array.push_back(array[i] * array[i]);
-    }
-    
-    if(!negativeNum)
-        return new_array;
+    int right_bound = array.size() - 1;
+    int left_bound = 0;
+    auto insert_it = new_array.end();
 
-    else {
-        sort(new_array.begin(), new_array.end());
-        return new_array;
-    }
+    while(left_bound <= right_bound) {
+
+        // cout << "left bound: " << array[left_bound] << " right bound: " << array[right_bound] << endl;
+
+        if(abs(array[left_bound]) > abs(array[right_bound])) {
+            // cout << "Add " << array[left_bound] * array[left_bound] << endl;
+            
+            insert_it = new_array.insert(insert_it, array[left_bound] * array[left_bound]);
+            left_bound++;
+        }
+
+        else {
+            // cout << "Add " << array[right_bound] * array[right_bound] << endl;
+
+            insert_it = new_array.insert(insert_it, array[right_bound] * array[right_bound]);
+            right_bound--;
+        }
+
+        // cout << "{";
+        // for(auto &item : new_array)
+        //     cout << item << ", ";
+        // cout << "}" << endl;
+
+        // cout << "Now insert_it points to: " << *insert_it << endl;
+        // cout << "-------------" << endl;
+
+    } // while    
+    
+    // new_array.insert(insert_it, array[right_bound] * array[right_bound]);
+
+    return new_array;
 } // sortedSquaredArray
 
 int main() {
 
-    vector<int> array_in = {1, 2, 3, 5, 6, 8, 9};
-    vector<int> array_out = sortedSquaredArray(array_in);
+    vector<int> array_in1 = {1, 2, 3, 5, 6, 8, 9};
+    vector<int> array_in2 = {-4, -2, 1, 5};
+    vector<int> array_out = sortedSquaredArray(array_in2);
     
     cout << "{";
     for(int i = 0; i < array_out.size(); ++i) {
